37b67faf3415fd4988a630b248bedbc61a4652ce
Albert Tamayo <n01408424@humber.ca>
2024-04-12 14:42:37 -0400

    main page

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://api.open-meteo.com/v1/forecast?latitude=43.7001&longitude=-79.4163&current=temperature_2m&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=America%2FNew_York')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.querySelector('.Up-to-date-weather').innerText = JSON.stringify(data.hourly_units.temperature_2m);
+    })
+    .catch(error => console.error('Error fetching data:', error))
